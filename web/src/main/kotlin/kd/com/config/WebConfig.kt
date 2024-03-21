4c85987a161b340bd4e91b2785fc2e8984e70ed2
Karuchiu <karuchiu8@gmail.com>
2024-03-21 13:40:04 +0300

    Thymeleaf + DevTools setup

---
+++ web/src/main/kotlin/kd/com/config/WebConfig.kt
@@ -0,0 +1,14 @@
+package kd.com.config
+
+import kd.com.util.ViewNames
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/").setViewName(ViewNames.HOME)
+    }
+}
---
+++ web/src/main/kotlin/kd/com/util/ViewNames.kt
@@ -0,0 +1,8 @@
+package kd.com.util
+
+class ViewNames private constructor() {
+
+    companion object{
+        const val HOME = "home"
+    }
+}
