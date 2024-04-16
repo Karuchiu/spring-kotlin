fbaaaceaf57f81b032045d0e073a7d2edd39dc3e
Karuchiu <karuchiu8@gmail.com>
2024-04-16 14:53:24 +0300

    LocaleChangeInterceptor

--- web/src/main/kotlin/kd/com/config/WebConfig.kt
+++ web/src/main/kotlin/kd/com/config/WebConfig.kt
@@ -2,13 +2,22 @@ package kd.com.config
 
 import kd.com.interceptor.RequestInterceptor
 import kd.com.util.ViewNames
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.LocaleResolver
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor
+import org.springframework.web.servlet.i18n.SessionLocaleResolver
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
+
+    @Bean
+    fun localeResolver(): LocaleResolver {
+        return SessionLocaleResolver()
+    }
 
     override fun addViewControllers(registry: ViewControllerRegistry) {
         registry.addViewController("/").setViewName(ViewNames.HOME)
@@ -16,5 +25,9 @@ class WebConfig : WebMvcConfigurer {
 
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(RequestInterceptor())
+
+        registry.addInterceptor(LocaleChangeInterceptor().apply {
+            paramName = "lang"
+        })
     }
 }
